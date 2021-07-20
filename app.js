3e3a3fbca77067ac70757ee5c498f8a87eedae37
trevor <me@trevorbehnke.com>
2021-07-19 22:28:24 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// Game Values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessLeft = 3;
+
+// UI elements
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+// Assign UI min max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  // Check if won
+  if (guess === winningNum) {
+    // Disable Input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = "green";
+    // Set message
+    setMessage(`${winningNum} is correct!`, "green");
+  } else {
+  }
+});
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
